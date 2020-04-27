8947df5eb89a8001d2d6034d6dad6ed461c41b68
Ksabah <khans3@tcd.ie>
2020-04-27 19:27:28 +0100

    Manual conversions needed to convert points

---
+++ perspectiveTrans.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2 as cv
+
+MIN_MATCH_COUNT = 10
+
+img1 = cv.imread('groundtruth/GroundTruth-69-halved.jpg',0) # queryImage
+img2 = cv.imread('images/original-images/Test2-pg69.jpg',0) # trainImage
+
+zones = np.load("zones/69-zones.npy")
+
+# Initiate SIFT detector
+sift = cv.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv.perspectiveTransform(pts,M)
+
+    img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+    zones = np.vstack(zones).squeeze()
+    zones = np.float32([zones])
+    pointsOut = cv.perspectiveTransform(zones, M)
+    cv.drawContours(img2, [pointsOut.astype(int)], -1, (0,255,0), 3)
+
+else:
+    print ("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+#img3 = cv.drawContours(img1,zones, -1, (0,255,0), 3)
+#img3 = cv.warpPerspective(img2, M, (img1.shape[1],img1.shape[0]))
+
+
+cv.namedWindow('Result 1', cv.WINDOW_NORMAL)
+cv.resizeWindow('Result 1', 1800,900)
+cv.imshow('Result 1',img3)
+cv.waitKey(0)
+cv.destroyAllWindows()
