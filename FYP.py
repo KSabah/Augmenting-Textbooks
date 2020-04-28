b3aada2201f8d500407c5302e3a9ddb6f8a1121b
Ksabah <khans3@tcd.ie>
2020-04-28 16:13:49 +0100

    All systems working

--- FYP.py
+++ FYP.py
@@ -11,35 +11,6 @@ from pageLinks import zone
 
 threshold_area = 8000     #threshold area for contours
 
-#load in zones for page 68
-img = cv.imread('groundtruth/GroundTruth-68-halved.jpg')
-data = np.load('zones/68-zones.npy')
-page68 = zone(img, data)
-links = ["https://www.cse.iitm.ac.in/~vplab/courses/CV_DIP/PDF/NEIGH_CONN.pdf", 
-        "http://www.imageprocessingplace.com/downloads_V3/root_downloads/tutorials/contour_tracing_Abeer_George_Ghuneim/connect.html",
-        "https://en.wikipedia.org/wiki/Connected-component_labeling", 
-        "https://nodepit.com/node/org.knime.knip.base.nodes.seg.ConnectedCompAnalysisNodeFactory%23Connected%20Component%20Analysis",
-        "https://aishack.in/tutorials/connected-component-labelling/",
-        "https://homepages.inf.ed.ac.uk/rbf/HIPR2/label.htm"]
-page68.insert(links) 
-
-#Load in zones for page 69
-img = cv.imread('groundtruth/GroundTruth-69-halved.jpg')
-data = np.load('zones/69-zones.npy')
-page69 = zone(img, data)
-links = ["https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html", 
-        "https://docs.opencv.org/3.4/d3/dc0/group__imgproc__shape.html",
-        "https://www.youtube.com/watch?v=hMIrQdX4BkE", "https://en.wikipedia.org/wiki/Connected-component_labeling"]
-page69.insert(links) 
-
-def putIterationsPerSec(frame, iterations_per_sec):
-    """
-    Add iterations per second text to lower-left corner of a frame.
-    """
-    cv.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
-        (10, 450), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
-    return frame
-
 def threadBoth(source=1):
     """
     Dedicated thread for grabbing video frames with VideoGet object.
@@ -79,7 +50,6 @@ def threadBoth(source=1):
             # make sure contour area is bigger than threshold, don't want little contours
             if area > threshold_area:
                 # determine the top-most extreme points along the contour 
-                # -> https://www.pyimagesearch.com/2016/04/11/finding-extreme-points-in-contours-with-opencv/
                 extTop = tuple(c[c[:, :, 1].argmin()][0])
                 # assign first value 
                 if (oldPoint is None):
@@ -88,11 +58,15 @@ def threadBoth(source=1):
                 if ((oldPoint[0]-5 <= extTop[0] <= oldPoint[0]+5) and (oldPoint[1]-5 <= extTop[1] <= oldPoint[1]+5)):
                     if (extTop[0] != 0 and extTop[1] != 0):
                         frameCount += 1
-                        #print("count: ", frameCount)
                         if frameCount == 20:
                             frameCount = 0
-                            #new = 2 opens in new tab, if possible
-                            #webbrowser.open('https://en.wikipedia.org/wiki/Connected-component_labeling', new=2)
+                            SIFT.run()
+                            #print([SIFT.newCoords.astype(int)])
+                            for i in range(len(SIFT.zone.coords)):
+                                res = cv.pointPolygonTest(SIFT.zone.coords[i], extTop, measureDist = False)
+                                print(res)
+                                if (res == 1):
+                                    webbrowser.open(SIFT.zone.zones[i][1], new=2)
                     oldPoint = extTop
                 else: 
                     frameCount = 0
--- SIFTThread.py
+++ SIFTThread.py
@@ -6,10 +6,35 @@ import pickle
 import time
 import threading
 
+from pageLinks import zone
+
+#load in zones for page 68
+img = cv.imread('groundtruth/GroundTruth-68-halved.jpg')
+data = np.load('zones/68-zones.npy')
+page68 = zone(img, data)
+links = ["https://www.cse.iitm.ac.in/~vplab/courses/CV_DIP/PDF/NEIGH_CONN.pdf", 
+        "http://www.imageprocessingplace.com/downloads_V3/root_downloads/tutorials/contour_tracing_Abeer_George_Ghuneim/connect.html",
+        "https://en.wikipedia.org/wiki/Connected-component_labeling", 
+        "https://nodepit.com/node/org.knime.knip.base.nodes.seg.ConnectedCompAnalysisNodeFactory%23Connected%20Component%20Analysis",
+        "https://aishack.in/tutorials/connected-component-labelling/",
+        "https://homepages.inf.ed.ac.uk/rbf/HIPR2/label.htm"]
+page68.insert(links) 
+
+#Load in zones for page 69
+img = cv.imread('groundtruth/GroundTruth-69-halved.jpg')
+data = np.load('zones/69-zones.npy')
+page69 = zone(img, data)
+links = ["https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html", 
+        "https://docs.opencv.org/3.4/d3/dc0/group__imgproc__shape.html",
+        "https://www.youtube.com/watch?v=hMIrQdX4BkE", "https://en.wikipedia.org/wiki/Connected-component_labeling"]
+page69.insert(links) 
+
 class SIFTThread():
     def __init__(self, frame):
         self.frame = frame
-        self.pagenum = 0        
+        self.pagenum = 0   
+        self.zone = None
+        self.newCoords = None
 
     def start(self):
         threading.Thread(target=self.run()).start()
@@ -18,14 +43,16 @@ class SIFTThread():
     def run(self):
         print("Running SIFT page recognition now...")
         sift = cv.xfeatures2d.SIFT_create()
-        frame_kp, frame_d = sift.detectAndCompute(self.frame, None)
+        frame = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
+        frame_kp, frame_d = sift.detectAndCompute(frame, None)
 
         #***Flann Matcher***
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
         search_params = dict(checks=50)  
         flann = cv.FlannBasedMatcher(index_params,search_params)
-
+        
+        MIN_MATCH_COUNT = 10
         good_matches = 0
         old_good_matches = 0
         result = ""
@@ -33,11 +60,13 @@ class SIFTThread():
         matches_result = None 
         result_kp = None
         result_d = None  
+        img2 = None
+        Matrix = None
 
         for image in os.listdir('groundtruth/'):
             str_arr = image.split("-")
             good_matches = 0
-            groundtruth = cv.imread('groundtruth/'+image) 
+            groundtruth = cv.imread('groundtruth/'+image, 0) 
                         
             if not os.path.exists("files/keypoints"+str_arr[1]+".txt"):
                 print ("Cannot find keypoints for groundtruth image, needs to be added to database")
@@ -51,13 +80,29 @@ class SIFTThread():
                 break
 
             matches = flann.knnMatch(groundtruth_d,frame_d,k=2)
-            #Mask for good matches
-            matchesMask = [[0,0] for i in range(len(matches))]
+            good = []
             #Ratio test 
             for i,(m,n) in enumerate(matches):
                 if m.distance < 0.5*n.distance:
-                    matchesMask[i]=[1,0]
                     good_matches += 1
+                    good.append(m)
+           
+            if len(good)>MIN_MATCH_COUNT:
+                src_pts = np.float32([ groundtruth_kp[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+                dst_pts = np.float32([ frame_kp[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+                Matrix, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+                matchesMask = mask.ravel().tolist()
+                
+                if (Matrix is not None):
+                    h, w = groundtruth.shape
+                    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+                    dst = cv.perspectiveTransform(pts,Matrix)
+                    img2 = cv.polylines(frame,[np.int32(dst)],True,255,3, cv.LINE_AA)
+
+            else:
+                print ("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
+                matchesMask = None
                     
             if good_matches > old_good_matches:
                 pagenum = str_arr[1]
@@ -68,10 +113,28 @@ class SIFTThread():
                 result_d = groundtruth_d
                     
         if(result_final is not None):
-            print("Result found")
-            cv.imwrite('SIFT_results/res.jpg', result_final)
             self.pagenum = pagenum
-            threading.Timer(20.0, lambda: self.start()).start()
+            print("Identified page: "+self.pagenum)
+            cv.imwrite('SIFT_results/result.jpg', result_final)
+
+            if(pagenum == "68"): 
+                self.zone = page68
+            else: self.zone = page69
+                       
+            try:
+                for i in range(len(self.zone.coords)):
+                    zone = np.vstack(self.zone.coords[i]).squeeze()
+                    zone = np.float32([zone])
+                    temp = cv.perspectiveTransform(zone, Matrix)
+                    self.zone.coords[i] = temp                  
+            except:
+                print("Something went wrong with transforming zones")
+                                   
+            #if (img2 is not None):
+                #img3 = cv.drawContours(img2, [self.zone.coords.astype(int)], -1, (0,255,0), 3)
+                #cv.imwrite('SIFT_results/perspective.jpg', img3)
+
+        threading.Timer(20.0, lambda: self.start()).start()
 
 def unpickle_keypoints(array):
     keypoints = []
